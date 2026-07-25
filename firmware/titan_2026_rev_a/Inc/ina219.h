9c3924ef7af7a471dc15d9cc9e742bcd6181b419
Savo Bajic <bajic.savo@gmail.com>
2026-07-25 23:34:43 +0100

    Removed redundant comments

--- firmware/titan_2026_rev_a/Inc/ina219.h
+++ firmware/titan_2026_rev_a/Inc/ina219.h
@@ -17,7 +17,7 @@ HAL_StatusTypeDef ina219_read_bus_voltage(const uint8_t INA_ADD, float * result_
 HAL_StatusTypeDef ina219_read_bus_current(const uint8_t INA_ADD, float * result_a);
 HAL_StatusTypeDef ina219_update_calibration(const uint8_t INA_ADD, float expected_max_current_a, float shunt_resistance_ohm);
 
-extern const uint8_t PRIM_INA; 	// Address for primary board's INA219
-extern const uint8_t SEC_INA;	// Address for secondary board's INA219
+extern const uint8_t PRIM_INA;
+extern const uint8_t SEC_INA;
 
 #endif /* INC_INA219_H_ */
